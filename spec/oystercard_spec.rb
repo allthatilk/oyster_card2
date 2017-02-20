a0769d94169f962853e95af08960e66b2bdfa6c1
Tahir Raza <taraza@tahirraza.com>
2017-02-20 16:16:57 +0000

    implimentation of balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
-describe do
-	
+require 'oyster_card'
+describe OysterCard do
+ subject(:card){described_class.new}	
+
+	describe "attributes" do
+     it "should check the balance" do
+     	expect(card.balance).to eq(0)
+     end
+	end 
 end
