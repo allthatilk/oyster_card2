3de546d7f224d40f02bde3c2bb03ab1966f617e6
Tahir Raza <taraza@tahirraza.com>
2017-02-20 16:44:31 +0000

    applying top_up

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -3,5 +3,8 @@ class OysterCard
 	def initialize(balance = 0)
 		@balance = balance
 	end
-			
+		def top_up(amount)
+		   @balance += amount
+		end 
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,16 @@ require 'oyster_card'
 describe OysterCard do
  subject(:card){described_class.new}	
 
-	describe "attributes" do
+	describe "attributes of oyster OysterCard" do
      it "should check the balance" do
      	expect(card.balance).to eq(0)
      end
+	end 
+	describe "top_up of OysterCard" do
+		it{is_expected.to respond_to(:top_up).with(1).argument}
+
+		it "should top up the balance" do
+			expect(card.top_up(10)).to eq(card.balance)
+		end 
 	end 
 end
