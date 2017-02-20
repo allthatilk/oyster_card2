9f419aee98c141b92d3a12218454f40b9b6861b2
Tahir Raza <taraza@tahirraza.com>
2017-02-20 17:34:43 +0000

    Setting the card maximum limit...

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,10 +1,21 @@
 class OysterCard 
+	MAXIMUM_BALANCE = 90
 	attr_reader :balance
 	def initialize(balance = 0)
 		@balance = balance
 	end
 		def top_up(amount)
-		   @balance += amount
+			 if limit_exceed(amount)  
+			 	fail "Card Limit exceeded"
+			 else
+			 	@balance += amount
+			 end
+			 return @balance
 		end 
+
+		def limit_exceed(amount)
+			@balance + amount > MAXIMUM_BALANCE
+
+		end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,8 +10,15 @@ describe OysterCard do
 	describe "top_up of OysterCard" do
 		it{is_expected.to respond_to(:top_up).with(1).argument}
 
+		it 'should raise error if balance>90' do
+			error = "Card Limit exceeded"
+			card.top_up(OysterCard::MAXIMUM_BALANCE)
+			# p card.balance.to_s + "Balance"
+			expect{card.top_up(1)}.to raise_error error
+		end 
+
 		it "should top up the balance" do
-			expect(card.top_up(10)).to eq(card.balance)
+			expect{card.top_up(10)}.to change{card.balance}.by 10
 		end 
 	end 
 end
