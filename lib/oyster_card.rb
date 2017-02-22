54cddda163b2bf353d948809004f70dcee14b62f
Tahir Raza <taraza@tahirraza.com>
2017-02-22 15:01:12 +0000

    Step 12 implementation

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,13 +1,17 @@
 class OysterCard 
 	MINIMUM_AMOUNT = 1
 	MAXIMUM_BALANCE = 90
-	attr_reader :balance, :entry_station, :exit_station
+	attr_reader :balance, :entry_station, :exit_station, :list_of_journey
 	def initialize(balance = 0)
-		@balance = balance
+		@balance = balance		
+		@list_of_journey = []
+
 	end
+		
 		def top_up(amount)
 			fail "Card Limit exceeded" if @balance + amount > MAXIMUM_BALANCE  			
-			@balance += amount			 
+			@balance += amount	
+
 		end 
 
 		def in_journey?	
@@ -20,8 +24,9 @@ class OysterCard
 			@entry_station  = entry_station 			
 		end
 		def touch_out?(station=nil)					
-			deduct(MINIMUM_AMOUNT)			
-			@entry_station = nil
+			deduct(MINIMUM_AMOUNT)
+			@list_of_journey << {entry: @entry_station, exit: station }
+			# @entry_station = nil
 			@exit_station = station
 		end
 		
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,6 +9,7 @@ describe OysterCard do
      	expect(card.balance).to eq(0)
      end
 	end 
+	
 	describe "top_up of OysterCard" do
 		it{is_expected.to respond_to(:top_up).with(1).argument}
 
@@ -21,33 +22,29 @@ describe OysterCard do
 		it "should top up the balance" do
 			expect{card.top_up(OysterCard::MINIMUM_AMOUNT)}.to change{card.balance}.by OysterCard::MINIMUM_AMOUNT
 		end 
-	end 
-	describe 'privious trips record' do
-
-		
+	end 	#top_up of oyster card
 	
-	end
 
 	describe 'in_journey' do
-		it {should respond_to(:in_journey?)}
-		it {should respond_to(:touch_in?)}
-		it {should respond_to(:touch_out?)}
-		
-		it "validates that card has the min_amout for journey" do
-			error = "Card doesn't have sufficiant money"
-			expect{card.touch_in?}.to raise_error error 
-		end
+		let(:balance){OysterCard::MAXIMUM_BALANCE}
+
+		context 'checking with 0 balance' do
+		let(:balance){0}
+			it "validates that card has the min_amout for journey" do
+				error = "Card doesn't have sufficiant money"
+				expect{card.touch_in?}.to raise_error error 
+			end
+		end	#contex
 		it 'validates card was not in journey' do			
 			expect(card).not_to be_in_journey
 		end
 	
 		it 'validates that card is deducted on touch out' do			
-			card.top_up(OysterCard::MAXIMUM_BALANCE)
 			card.touch_in?
 			expect{card.touch_out?}.to change{card.balance}.by(-OysterCard::MINIMUM_AMOUNT)
 		end
 
-	end	
+	end		#in_journey
 	describe 'station' do
 		let(:entry_station) { :aldgate_east }
 		let(:ex_st) { double(:exit_station) }
@@ -59,16 +56,23 @@ describe OysterCard do
 		end
 		it 'confirms card has an exit station when touch_out' do
 			card.touch_in?(:entry_station)
-			card.touch_out?(:ex_st)
-			#card.touch_in?
+			card.touch_out?(:ex_st)		
 			expect(card.exit_station).to eq :ex_st
 		end
-		# it 'adsdasda' do
-		# 	card.touch_in?(:entry_station)
-		# end
 
-
-
-
-	end
-end
+	end		#station
+	describe 'trips record' do
+		let(:balance){1}
+		#let(:journey){ {} }
+		it 'should confirm it has empty list of journey' do
+			expect(card.list_of_journey).to eq []
+		end
+		it 'confirm touch_in & touch_out creates journey' do			
+			card.touch_in?('entry')
+			card.touch_out?('exit')
+			expect(card.list_of_journey).to eq([{ entry:'entry', exit: 'exit'}])
+		end
+		
+	
+	end	# trips record
+end		#OysterCard
