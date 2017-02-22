479211d611470ceac61cccd40bce769bb15290a1
allthatilk <imogenkutz@hotmail.com>
2017-02-22 22:31:10 +0000

    Tidied tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,9 @@
 require 'oyster_card'
 describe OysterCard do
- let(:balance) {0}
- subject(:card){described_class.new(balance)}
+ let(:balance) { 0 }
+ subject(:card) { described_class.new(balance) }
+ min_amount = OysterCard::MINIMUM_AMOUNT
+ max_amount = OysterCard::MAXIMUM_BALANCE
 
 
 	describe "attributes of OysterCard" do
@@ -13,44 +15,46 @@ describe OysterCard do
 	describe "top_up of OysterCard" do
 		it{is_expected.to respond_to(:top_up).with(1).argument}
 
-		it 'should raise error if balance>90' do
-			error = "Card Limit exceeded"
-			card.top_up(OysterCard::MAXIMUM_BALANCE)
-			expect{card.top_up(1)}.to raise_error error
-		end
+    context "balance max error" do
+      let(:balance) { max_amount }
+  		it 'should raise error if balance>90' do
+  			error = "Card Limit exceeded"
+  			expect{ card.top_up(min_amount) }.to raise_error error
+  		end
+    end
 
 		it "should top up the balance" do
-			expect{card.top_up(OysterCard::MINIMUM_AMOUNT)}.to change{card.balance}.by OysterCard::MINIMUM_AMOUNT
+			expect{ card.top_up(min_amount) }.to change{ card.balance }.by min_amount
 		end
 	end 	#top_up of oyster card
 
 
 	describe 'in_journey' do
-		let(:balance){OysterCard::MAXIMUM_BALANCE}
+		let(:balance){ max_amount }
 
 		context 'checking with 0 balance' do
-		let(:balance){0}
+		let(:balance){ 0 }
 			it "validates that card has the min_amout for journey" do
 				error = "Card doesn't have sufficiant money"
 				expect{card.touch_in?}.to raise_error error
 			end
-		end	#contex
-		it 'validates card was not in journey' do
-			expect(card).not_to be_in_journey
-		end
+		end	#context
+
+  	it 'validates card was not in journey' do
+  		expect(card).not_to be_in_journey
+  	end
 
 		it 'validates that card is deducted on touch out' do
 			card.touch_in?
-			expect{card.touch_out?}.to change{card.balance}.by(-OysterCard::MINIMUM_AMOUNT)
+			expect{card.touch_out?}.to change{card.balance}.by(-min_amount)
 		end
-
 	end		#in_journey
 
 
 	describe 'station' do
 		let(:entry_station) { :aldgate_east }
-		let(:ex_st) { double(:exit_station) }
-		let(:balance){1}
+		let(:exit_station) { :exit_station }
+		let(:balance){ min_amount }
 		it 'confirms card has an entry station on touch_in' do
 			expect(card.touch_in?(:entry_station)).to eq :entry_station
 		end
@@ -58,7 +62,5 @@ describe OysterCard do
 			card.touch_in?(:entry_station)
 			expect(card.touch_out?(:ex_st)).to eq :ex_st
 		end
-
 	end		#station
-	
 end		#OysterCard
